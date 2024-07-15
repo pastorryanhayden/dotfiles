e043ea107a8c3379b1b5b4ae0faaeebaee89cadd
Ryan Hayden <pastorryanhayden@gmail.com>
2024-07-15 15:45:46 -0500

    made the background transparent

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -3,9 +3,10 @@
 export WHITE=0xffffffff
 
 # -- Teal Scheme --
-export BAR_COLOR=0xff142E42
+export BAR_COLOR=0x00142E42
 export ITEM_BG_COLOR=0xff3b4261
 export ACCENT_COLOR=0xff7dcfff
+export TEXT_COLOR=0xFF142E42
 
 # -- Gray Scheme --
 # export BAR_COLOR=0xff101314
--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -7,12 +7,12 @@
 source "$CONFIG_DIR/colors.sh" # Loads all defined colors
 
 if [ $SELECTED = true ]; then
-  sketchybar --set $NAME background.drawing=on \
-    background.color=$ACCENT_COLOR \
-    label.color=$BAR_COLOR \
-    icon.color=$BAR_COLOR
+	sketchybar --set $NAME background.drawing=on \
+		background.color=$ACCENT_COLOR \
+		label.color=$TEXT_COLOR \
+		icon.color=$TEXT_COLOR
 else
-  sketchybar --set $NAME background.drawing=off \
-    label.color=$ACCENT_COLOR \
-    icon.color=$ACCENT_COLOR
+	sketchybar --set $NAME background.drawing=off \
+		label.color=$ACCENT_COLOR \
+		icon.color=$ACCENT_COLOR
 fi
